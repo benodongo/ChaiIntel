ad95a0bcee6b2085091530fd1736cd0452fc19ef
Benson Odongo <2013odongo@gmail.com>
2025-06-18 17:12:10 +0300

    settings

--- ChaiIntel/settings.py
+++ ChaiIntel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-soa4vlhu-*z*k4@3sui43vll+&ad(37yv8_qiy^h=#!12nxw*i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chaiintel.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
